40aff1580277db537e807e183540f8c21bf97f18
tostyle <tostyle@gmail.com>
2026-04-28 10:51:41 +0000

    add ansible script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 echo "==> Linking tmux config..."
 ln -sf "$(pwd)/.tmux.config" "$HOME/.tmux.conf"
+tmux source-file ~/.tmux.conf
 
 echo "==> Applying home-manager configuration..."
 nix run nixpkgs#home-manager -- switch --flake .#coder
