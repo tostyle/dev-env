e5d6fd13eaa06cc65488d0e3315caa309ce23d95
tostyle <tostyle>
2026-05-21 21:14:17 +0700

    Add direnv

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 echo "==> Linking tmux config..."
 ln -sf "$(pwd)/.tmux.config" "$HOME/.tmux.conf"
 
+
 echo "==> Applying home-manager configuration..."
 nix run nixpkgs#home-manager -- switch --flake .#coder
 
